a96a4caf5856dc4220b1a74176dacca85c7c8ea9
thiago.pereira <thiago.pereira@doc88.com.br>
2024-05-14 22:48:28 -0300

    corriginda dados de conexao

--- services/conexao.php
+++ services/conexao.php
@@ -1,20 +1,22 @@
 <?php
-    // Configurações do banco de dados
-    $servidor = "localhost";
-    $db = "uninove";
+    // Configuração do banco de dados
+    $servidor = "roundhouse.proxy.rlwy.net";
+    $db = "railway";
     $usuario = "root";
-    $senha = "";
+    $senha = "EaTPXTrsgvyOLhwvSLLCybxNSnJqvOlb";
+    $porta = "21342";
 
     // Tentar estabelecer a conexão PDO
     try {
         // Cria uma nova conexão PDO
-        $pdo = new PDO("mysql:host=$servidor;dbname=$db", $usuario, $senha);
-        
+        $pdo = new PDO("mysql:host=$servidor;port=$porta;dbname=$db", $usuario, $senha);
+
         // Define o modo de erro do PDO para exceções
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $pdo->setAttribute(PDO::ATTR_TIMEOUT, 300); 
     } catch (PDOException $e) {
         // Em caso de falha na conexão, exibe uma mensagem de erro
         die("Falha na conexão: " . $e->getMessage());
     }
 ?>
-
